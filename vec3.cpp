44413d8ea2d8fd76a48d3106da31a060520dfa65
Daryl-Kay <72775788+Daryl-Kay@users.noreply.github.com>
2021-12-30 13:17:24 -0600

    vec3 class without utility part

--- vec3.cpp
+++ vec3.cpp
@@ -17,16 +17,42 @@ class vec3 {
             cout << e[0] << e[1] << e[2];
         }
 
-        //indexing
-        //address
-        //xyz
-        //negative
-        // divide
-        //multiply operator
-        //add operator (two vectors)
-        //length function
-        //length squared function
 
+        double operator[](int i) const { return e[i]; }
+        double& operator[](int i) { return e[i]; }
+
+        double x() const { return e[0]; }
+        double y() const { return e[1]; }
+        double z() const { return e[2]; }
+
+        vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+
+        vec3& operator*=(const double t) {
+            e[0] *= t;
+            e[1] *= t;
+            e[2] *= t;
+            return *this;
+        }
+
+        vec3& operator/=(const double t) {
+            return *this *= 1/t;
+        }
+
+
+        vec3& operator+=(const vec3& v) {
+            e[0] += v.e[0];
+            e[1] += v.e[1];
+            e[2] += v.e[1];
+            return *this;
+        }
+
+        double length() const {
+            return sqrt(lengthSquared());
+        }
+
+        double lengthSquared() const {
+            return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+        }
 };
 
 using point3 = vec3;
