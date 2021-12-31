35c72cd1c52c785ebc85a1e2cc08958eb01830ab
Daryl-Kay <72775788+Daryl-Kay@users.noreply.github.com>
2021-12-31 00:03:54 -0600

    creating image from rays from a camera rather than just i and j

--- main.cpp
+++ main.cpp
@@ -6,9 +6,17 @@
 
 using namespace std;
 
+color rayColor(const ray &r) {
+    vec3 unitDirection = unitVector(r.direction());
+    double t = 0.5*(unitDirection.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
+
 int main() {
-    const int imageWidth = 256;
-    const int imageHeight = 256;
+    //image
+    const double aspectRatio = 16.0/9.0;
+    const int imageWidth = 400;
+    const int imageHeight = (int)(imageWidth / aspectRatio);
 
     ofstream image;
     image.open("image.ppm", ios::out);
@@ -17,18 +25,30 @@ int main() {
     image << imageWidth << ' ' << imageHeight << endl;
     image << "255" << endl;
 
+    //camera
+    double viewportHeight = 2.0;
+    double viewportWidth = viewportHeight * aspectRatio;
+    double focalLength = 1.0;
+
+    point3 origin = point3(0,0,0);
+    vec3 horizontal = vec3(viewportWidth, 0, 0);
+    vec3 vertical = vec3(0, viewportHeight, 0);
+    point3 lowerLeftCorner = origin - horizontal/2 - vertical/2 - vec3(0,0,focalLength);
+
     for (int j = imageHeight - 1; j >= 0; j--) {
         cerr << "\rScanlines remaining: " << j << ' ' << flush;
         for (int i = 0; i < imageWidth; i++) {
-            color pixel(((double)i / (imageHeight - 1)),
-                        ((double)j / (imageWidth - 1)),
-                        0.25);
+            double u = double(i) / (imageWidth - 1);
+            double v = double(j) / (imageHeight - 1);
+
+            ray r(origin, lowerLeftCorner + u*horizontal + v*vertical - origin);
+            color pixel = rayColor(r);
 
             writeColor(image, pixel);
         }
     }
 
-    cout << "Done!" << endl;
+    cout << "\nDone!" << endl;
     image.close();
     return 0;
 }
