2224d39411cbd21c4166bdd18f49fc2863e6f4b6
Daryl-Kay <72775788+Daryl-Kay@users.noreply.github.com>
2022-01-31 23:28:36 -0600

    done for today

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ color rayColor(const ray& r, const hittable& world, const int reflectLeft) {
         return color(0,0,0);
     }
 
-    if(world.hit(r, 0, infinity, rec)){
+    if(world.hit(r, 0.000001, infinity, rec)){
         vec3 bounce = rec.p + rec.normal + randomUnitSphere();
         return 0.5 * (rayColor(ray(rec.p, bounce - rec.p), world, reflectLeft - 1));
     }
@@ -28,10 +28,22 @@ color rayColor(const ray& r, const hittable& world, const int reflectLeft) {
 int main() {
     camera cam;
 
+    const int imageOption = 2; //higher = higher res
+    int numSamples = 1000;
+
+    int imageWidth;
+    if (imageOption == 3) {
+        imageWidth = 1920;
+    } else if (imageOption == 2) {
+        imageWidth = 1280;
+    } else if (imageOption == 1) {
+        imageWidth = 720;
+    } else {
+        imageWidth = 400;
+    }
+
     const double aspectRatio = 16.0/9.0;
-    const int imageWidth = 400;
     const int imageHeight = (int)(imageWidth / aspectRatio);
-    int numSamples = 5;
     int maxBounce = 50;
 
     ofstream image;
@@ -45,18 +57,26 @@ int main() {
     hittable_list world;
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
     world.add(make_shared<sphere>(point3(0,-1000.5,-1),1000));
+    world.add(make_shared<sphere>(point3(-0.45,0,-1),0.25));
+    world.add(make_shared<sphere>(point3(0.45,0,-1),0.25));
+    world.add(make_shared<sphere>(point3(-0.15,0.05,-0.5),0.1));
+    world.add(make_shared<sphere>(point3(0.15,0.05,-0.5),0.1));
+    world.add(make_shared<sphere>(point3(-2.25,0,-2),0.5));
+    world.add(make_shared<sphere>(point3(2.25,0,-2),0.5));
 
-    static color imageDS[imageWidth][imageHeight];
 
-    for(int samp = 0; samp < numSamples; ++samp) {
-        cerr << "\rSamples done: " << samp << "/" << numSamples << flush;
-        for (int j = imageHeight - 1; j >= 0; j--) {
-            for (int i = 0; i < imageWidth; i++) {
-                double u = (i + randomDouble()) / (imageWidth - 1.0);
-                double v = (j + randomDouble()) / (imageHeight - 1.0);
+
+    for (int j = imageHeight-1; j >= 0; --j) {
+        cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < imageWidth; ++i) {
+            color pixelColor(0, 0, 0);
+            for (int s = 0; s < numSamples; ++s) {
+                double u = (i + randomDouble()) / (imageWidth-1);
+                double v = (j + randomDouble()) / (imageHeight-1);
                 ray r = cam.getRay(u, v);
-                imageDS[i][j] = imageDS[i][j] + rayColor(r, world, maxBounce);
+                pixelColor += rayColor(r, world, maxBounce);
             }
+            writeColor(image, pixelColor, numSamples);
         }
     }
 
